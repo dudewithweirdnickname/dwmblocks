fc7c05e6fb2a1f461400beb227da9a8a83d754f5
dudewithweirdnickname <hacusimo@gmail.com>
2020-03-26 02:51:13 +0200

    playing with spaces

--- blocks.h
+++ blocks.h
@@ -2,17 +2,17 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	
-	{"RAM: ", "free -h | awk '/^Mem/ { print $3 }' | sed s/i//g",	30,		0},
+	{" RAM:  ", "free -h | awk '/^Mem/ { print $3 }' | sed s/i//g",	30,		0},
 
-	{"", "status-vol.sh",						0,		10},
+	{" ", "status-vol.sh",						0,		10},
 
-	{"X", "xbacklight | cut -b -2",					0,		11},
+	{" X:  ", "xbacklight | cut -b -2",					0,		11},
 
-	{"CPU: ",   "sensors | awk '/^temp1:/{print $2}'",				5,		0},
+	{" CPU:  ",   "sensors | awk '/^temp1:/{print $2}'",				5,		0},
 
-	{"BAT2: ", "status-bat0.sh",						5,		0},
+	{" BAT2:  ", "status-bat0.sh",						5,		0},
 
-	{"BAT1: ", "status-bat1.sh",						5,		0},
+	{" BAT1:  ", "status-bat1.sh",						5,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
